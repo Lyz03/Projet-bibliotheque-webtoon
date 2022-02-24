551294aa78c6c611d4f5837840b32ecf56134921
Lyz03 <lizoe@netcourrier.com>
2022-02-24 09:57:06 +0100

    input focus

--- public/index.php
+++ public/index.php
@@ -7,8 +7,7 @@ $page = strip_tags($page);
  * @param string $page
  * @param string $title
  */
-function getPage(string $page, string $title): void
-{
+function getPage(string $page, string $title): void {
     require __DIR__ . '/../parts/header.php';
     require file_exists($page) ? $page : __DIR__ . '/../pages/404.php';
     require __DIR__ . '/../parts/footer.php';
